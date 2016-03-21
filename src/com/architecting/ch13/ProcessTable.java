9af4a6e2820d63f73b894284f0137ce7d9814030
ArchitectingHBase <ArchitectingHBase@gmail.com>
2016-03-20 19:28:45 -0700

    Ch13: Add asciidoc tags and formating

--- src/com/architecting/ch13/ProcessTable.java
+++ src/com/architecting/ch13/ProcessTable.java
@@ -10,12 +10,9 @@ import org.apache.hadoop.hbase.client.Result;
 import org.apache.hadoop.hbase.client.Scan;
 import org.apache.hadoop.hbase.filter.KeyOnlyFilter;
 import org.apache.hadoop.hbase.io.ImmutableBytesWritable;
-import org.apache.hadoop.hbase.mapreduce.TableInputFormat;
-import org.apache.hadoop.hbase.spark.HBaseContext;
 import org.apache.hadoop.hbase.spark.JavaHBaseContext;
 import org.apache.hadoop.hbase.util.Bytes;
 import org.apache.spark.SparkConf;
-import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.api.java.function.FlatMapFunction;
@@ -25,78 +22,91 @@ import scala.Tuple2;
 
 public class ProcessTable {
 
-  public static final TableName TABLE_NAME = TableName.valueOf("user");
-  public static final byte[] COLUMN_FAMILY = Bytes.toBytes("segment");
+public static final TableName TABLE_NAME = TableName.valueOf("user");
+public static final byte[] COLUMN_FAMILY = Bytes.toBytes("segment");
 
-  @SuppressWarnings("serial")
-  public static void processVersion1() {
-    // tag::BULKPUT[]
-    SparkConf sc = new SparkConf().setAppName("IngestLines ").setMaster("local[2]");
-    JavaSparkContext jsc = new JavaSparkContext(sc);
-    Configuration conf = HBaseConfiguration.create();
+@SuppressWarnings("serial")
+public static void processVersion1() {
+  // tag::INIT[]
+  SparkConf sc = new SparkConf().setAppName("ProcessTable").setMaster("local[2]");
+  JavaSparkContext jsc = new JavaSparkContext(sc);
+  Configuration conf = HBaseConfiguration.create();
 
-    JavaHBaseContext hbaseContext = new JavaHBaseContext(jsc, conf);
-     
-    Scan scan = new Scan();
-    scan.setCaching(100);
-    KeyOnlyFilter kof = new KeyOnlyFilter();
-    scan.setFilter(kof);
-         
-    JavaRDD<Tuple2<ImmutableBytesWritable, Result>> data = hbaseContext.hbaseRDD(TableName.valueOf("user"), scan);
-    FlatMapFunction<Iterator<Tuple2<ImmutableBytesWritable, Result>>, Integer> setup = new FlatMapFunction<Iterator<Tuple2<ImmutableBytesWritable, Result>>, Integer>() {
-      @Override
-      public Iterable<Integer> call(Iterator<Tuple2<ImmutableBytesWritable, Result>> input) {
-        int a = 0;
-        while (input.hasNext()) {
-          a++;
-          input.next();
-        }
-        ArrayList<Integer> ret = new ArrayList<Integer>();
-        ret.add(a);
-        return ret;
+  JavaHBaseContext hbaseContext = new JavaHBaseContext(jsc, conf);
+   
+  Scan scan = new Scan();
+  scan.setCaching(100);
+  KeyOnlyFilter kof = new KeyOnlyFilter();
+  scan.setFilter(kof);
+       
+  JavaRDD<Tuple2<ImmutableBytesWritable, Result>> data =
+                          hbaseContext.hbaseRDD(TableName.valueOf("user"), scan);
+  // end::INIT[]
+  
+  long time1 = System.currentTimeMillis();
+  System.err.println("data.count() = " + data.count());
+  long time2 = System.currentTimeMillis();
+  System.err.println("Took " + (time2 - time1) + " milliseconds");
+  
+  // tag::PROCESS1[] 
+  FlatMapFunction<Iterator<Tuple2<ImmutableBytesWritable, Result>>, Integer> setup =
+  new FlatMapFunction<Iterator<Tuple2<ImmutableBytesWritable, Result>>, Integer>() {
+    @Override
+    public Iterable<Integer>
+                      call(Iterator<Tuple2<ImmutableBytesWritable, Result>> input) {
+      int a = 0;
+      while (input.hasNext()) {
+        a++; // <1> 
+        input.next();
       }
-    };
-    Function2<Integer, Integer, Integer> combine = new Function2<Integer, Integer, Integer>() {
-      @Override
-      public Integer call(Integer a, Integer b) {
-        return a+b;
-      }
-    };
+      ArrayList<Integer> ret = new ArrayList<Integer>();
+      ret.add(a);
+      return ret;
+    }
+  };
+  Function2<Integer, Integer, Integer> combine =
+  new Function2<Integer, Integer, Integer>() {
+    @Override
+    public Integer call(Integer a, Integer b) {
+      return a+b; // <2>
+    }
+  };
+  
+  System.err.println("data.mapPartitions(setup).reduce(combine) = " + 
+      data.mapPartitions(setup).reduce(combine));
+  long time3 = System.currentTimeMillis();
+  System.err.println("Took " + (time3 - time2) + " milliseconds");
+  // end::PROCESS1[]
+
+  
+  // tag::PROCESS2[] 
+  Function2<Integer, Tuple2<ImmutableBytesWritable, Result>, Integer> aggregator =
+  new Function2<Integer, Tuple2<ImmutableBytesWritable, Result>, Integer>() {
+    @Override
+    public Integer call(Integer v1, Tuple2<ImmutableBytesWritable, Result> v2)
+        throws Exception {
+      return v1 + 1; // <1>
+    }
+  };
+  Function2<Integer, Integer, Integer> combiner =
+  new Function2<Integer, Integer, Integer>() {
+    @Override
+    public Integer call(Integer v1, Integer v2) throws Exception {
+      return v1 + v2; // <2>
+    }
+  };
+  
+  System.err.println("data.aggregate(0, aggregator, combiner) = " + 
+                                           data.aggregate(0, aggregator, combiner));
+  long time4 = System.currentTimeMillis();
+  System.err.println("Took " + (time4 - time3) + " milliseconds");
+  // end::PROCESS1[]
     
-    Function2<Integer, Tuple2<ImmutableBytesWritable, Result>, Integer> aggregator = new Function2<Integer, Tuple2<ImmutableBytesWritable, Result>, Integer>() {
-      @Override
-      public Integer call(Integer v1, Tuple2<ImmutableBytesWritable, Result> v2) throws Exception {
-        // TODO Auto-generated method stub
-        return v1 + 1;
-      }
-    };
-    Function2<Integer, Integer, Integer> combiner = new Function2<Integer, Integer, Integer>() {
+  jsc.close();
+}
 
-      @Override
-      public Integer call(Integer v1, Integer v2) throws Exception {
-        // TODO Auto-generated method stub
-        return v1 + v2;
-      }
-
-    };
-    
-    long time1 = System.currentTimeMillis();
-    System.out.println("data.count() = " + data.count());
-    long time2 = System.currentTimeMillis();
-    System.out.println("Took " + (time2 - time1) + " milliseconds");
-    System.out.println("data.aggregate(0, aggregator, combiner) = " + data.aggregate(0, aggregator, combiner));
-    long time3 = System.currentTimeMillis();
-    System.out.println("Took " + (time3 - time2) + " milliseconds");
-    System.out.println("data.mapPartitions(setup).reduce(combine) = " + data.mapPartitions(setup).reduce(combine));
-    long time4 = System.currentTimeMillis();
-    System.out.println("Took " + (time4 - time2) + " milliseconds");
-    
-    jsc.close();
-    // end::BULKPUT[]
-  }
-
-  public static void main(String[] args) {
-    processVersion1();
-  }
+public static void main(String[] args) {
+  processVersion1();
+}
 
 }
